87548111faeb4935af46e41d8e7bf10e27a112a7
ramjeesh7@gmail.com <ramjeesh7@gmail.com>
2024-06-01 21:59:30 -0400

    second commit

--- sql.py
+++ sql.py
@@ -1,1 +1,28 @@
-test 
+#import sqlite3
+import sqlite3
+
+#define connection and cursor
+
+conn = sqlite3.connect('Movies.db')
+
+cursor = conn.cursor()
+
+#define commands and execute commands
+command1 = 'SELECT * FROM Top_50'
+command2 = 'SELECT COUNT(*) FROM Top_50'
+
+
+cursor.execute(command1)
+
+# # Query statement	                            Purpose
+# # SELECT * FROM table_name	                Retrieve all entries of the table.
+# # SELECT COUNT(*) FROM table_name	            Retrieve total number of entries in the table.
+# # SELECT Column_name FROM table_name	        Retrieve all entries of a specific column in the table.
+# # SELECT * FROM table_name WHERE <condition>	Retrieve all entries of the table that meet the specified condition.
+
+#store results in a variable and print the result
+results = cursor.fetchall()
+print(results[0])
+
+#close the connection
+conn.close()
